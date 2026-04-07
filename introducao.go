313e8e85af09f7c9a878603b31cb7e237f78a37e
adm <kaio.candf@gmail.com>
2026-04-07 15:40:43 -0300

    dando inicio

---
+++ introducao.go
@@ -0,0 +1,8 @@
+package main
+
+import enderecos "introducaoTestes/Testes/Introducao/Enderecos"
+
+func main() {
+	tipoEndereco := enderecos.TipoDeEndereco("Avenida Paulista")
+	println(tipoEndereco)
+}
